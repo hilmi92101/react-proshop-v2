8382fd01e6857aaabca43f18275b00a7f6bdce44
hilmi <muhammadhilmi92101@gmail.com>
2023-12-15 21:34:46 +0800

    refactor router

---
+++ frontend/src/modules/router.js
@@ -0,0 +1,24 @@
+//import React from 'react';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    //RouterProvider,
+} from 'react-router-dom';
+
+import App from '../App';
+
+// screens
+import HomeScreen from '../screens/HomeScreen';
+import ProductScreen from '../screens/ProductScreen';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route index={true} path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+        </Route>
+    )
+);
+
+export default router;
