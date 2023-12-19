189917a63f6751234eb040045f0d9aeb8d942dbf
hilmi <muhammadhilmi92101@gmail.com>
2023-12-19 22:05:30 +0800

    add user routes, user controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -13,6 +14,7 @@ connectDB();
 
 const app = express();
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.get("/", (req, res) => {
     res.send('API is running...');
 });
