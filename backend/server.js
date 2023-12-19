98e71300cc874b79a121bd90e74da0cb17cc2392
hilmi <muhammadhilmi92101@gmail.com>
2023-12-20 00:33:14 +0800

    add login function

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+// packages
 import express from 'express';
 import dotenv from 'dotenv';
+
+// connection
 import connectDB from './config/db.js';
 
+// routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
+// middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -12,7 +17,13 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
+// app
 const app = express();
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.get("/", (req, res) => {
